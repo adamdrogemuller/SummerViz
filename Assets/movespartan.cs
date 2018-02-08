0eeff7a62d7369c226471cfe1f2e42a965bca54a
Gokul Sidarth <gthiruna@deakin.edu.au>
2018-02-08 15:09:47 +1100

    commit message

---
+++ Assets/movespartan.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movespartan : MonoBehaviour {
+	public Transform[] target;
+	public float speed;
+	private Vector3 startingPosition;
+	private int i = 0;
+	// Use this for initialization
+	void Start () {
+		startingPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (i < target.Length) {
+			if (Vector3.Distance (transform.position, target [i].position) > 0) {
+				transform.position = Vector3.MoveTowards (transform.position, target [i].position, speed * Time.deltaTime);
+			} else {
+				i++;
+			}
+		} else {
+			transform.position = startingPosition;
+			i = 0;
+		}
+	}
+}
